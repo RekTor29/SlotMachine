f018c45d0418e8b0a963269454d6b9a661a3a8ec
RekTor29 <alessio.rizzo.studenti@isii.it>
2020-12-20 21:51:56 +0100

    Aggiungere i file di progetto.

---
+++ SlotMachineV1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace SlotMachineV1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.bgwRullo1 = new System.ComponentModel.BackgroundWorker();
+            this.bgwRullo2 = new System.ComponentModel.BackgroundWorker();
+            this.bgwRullo3 = new System.ComponentModel.BackgroundWorker();
+            this.pctTitolo = new System.Windows.Forms.PictureBox();
+            this.pctRullo2 = new System.Windows.Forms.PictureBox();
+            this.pctRullo3 = new System.Windows.Forms.PictureBox();
+            this.pctRullo1 = new System.Windows.Forms.PictureBox();
+            this.btnRisultati = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTitolo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRullo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRullo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRullo1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Ravie", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(249, 179);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(198, 51);
+            this.btnStop.TabIndex = 16;
+            this.btnStop.Text = "button2";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Ravie", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 179);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(198, 51);
+            this.btnStart.TabIndex = 15;
+            this.btnStart.Text = "button1";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // bgwRullo1
+            // 
+            this.bgwRullo1.WorkerReportsProgress = true;
+            this.bgwRullo1.WorkerSupportsCancellation = true;
+            this.bgwRullo1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwRullo1_DoWork);
+            // 
+            // bgwRullo2
+            // 
+            this.bgwRullo2.WorkerReportsProgress = true;
+            this.bgwRullo2.WorkerSupportsCancellation = true;
+            this.bgwRullo2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwRullo2_DoWork);
+            // 
+            // bgwRullo3
+            // 
+            this.bgwRullo3.WorkerReportsProgress = true;
+            this.bgwRullo3.WorkerSupportsCancellation = true;
+            this.bgwRullo3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgwRullo3_DoWork);
+            // 
+            // pctTitolo
+            // 
+            this.pctTitolo.Location = new System.Drawing.Point(12, 4);
+            this.pctTitolo.Name = "pctTitolo";
+            this.pctTitolo.Size = new System.Drawing.Size(434, 63);
+            this.pctTitolo.TabIndex = 17;
+            this.pctTitolo.TabStop = false;
+            // 
+            // pctRullo2
+            // 
+            this.pctRullo2.Location = new System.Drawing.Point(159, 73);
+            this.pctRullo2.Name = "pctRullo2";
+            this.pctRullo2.Size = new System.Drawing.Size(141, 96);
+            this.pctRullo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctRullo2.TabIndex = 14;
+            this.pctRullo2.TabStop = false;
+            // 
+            // pctRullo3
+            // 
+            this.pctRullo3.Location = new System.Drawing.Point(306, 73);
+            this.pctRullo3.Name = "pctRullo3";
+            this.pctRullo3.Size = new System.Drawing.Size(141, 96);
+            this.pctRullo3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctRullo3.TabIndex = 13;
+            this.pctRullo3.TabStop = false;
+            // 
+            // pctRullo1
+            // 
+            this.pctRullo1.Location = new System.Drawing.Point(12, 73);
+            this.pctRullo1.Name = "pctRullo1";
+            this.pctRullo1.Size = new System.Drawing.Size(141, 96);
+            this.pctRullo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctRullo1.TabIndex = 12;
+            this.pctRullo1.TabStop = false;
+            // 
+            // btnRisultati
+            // 
+            this.btnRisultati.Font = new System.Drawing.Font("Ravie", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRisultati.Location = new System.Drawing.Point(348, 236);
+            this.btnRisultati.Name = "btnRisultati";
+            this.btnRisultati.Size = new System.Drawing.Size(99, 27);
+            this.btnRisultati.TabIndex = 18;
+            this.btnRisultati.Text = "button1";
+            this.btnRisultati.UseVisualStyleBackColor = true;
+            this.btnRisultati.Click += new System.EventHandler(this.btnRisultati_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 275);
+            this.Controls.Add(this.btnRisultati);
+            this.Controls.Add(this.pctTitolo);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pctRullo2);
+            this.Controls.Add(this.pctRullo3);
+            this.Controls.Add(this.pctRullo1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "$ Lucky Slot $";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctTitolo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRullo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRullo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRullo1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pctRullo2;
+        private System.Windows.Forms.PictureBox pctRullo3;
+        private System.Windows.Forms.PictureBox pctRullo1;
+        private System.ComponentModel.BackgroundWorker bgwRullo1;
+        private System.ComponentModel.BackgroundWorker bgwRullo2;
+        private System.ComponentModel.BackgroundWorker bgwRullo3;
+        private System.Windows.Forms.PictureBox pctTitolo;
+        private System.Windows.Forms.Button btnRisultati;
+    }
+}
+
---
+++ SlotMachineV1/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace SlotMachineV1
+{
+    public partial class frmMain : Form
+    {
+        //Volevo confrontare le immagini dentro le pct per verificare un ipotetico jackpot
+        //Utilizzando un ipotetico If(pctbox1.Image == pctbox2.Image), tuttavia non mi è riuscito..
+        //Quindi ho dichiarato 3 Int che conterranno i random per i 3 diversi rulli
+        //Così da confrontarli alla fine dell'ultimo while e verificare se è uscito il Jackpot
+        int NumRullo1 = 0;
+        int NumRullo2 = 0;
+        int NumRullo3 = 0;
+        Random rnd = new Random();
+        int numpartite = 0;
+        int numvittorie = 0;
+        Stopwatch tempopartita = new Stopwatch();
+       
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnStart.Text = "Start";
+            btnStop.Text = "Stop";
+            btnRisultati.Text = "Statistiche";
+            this.BackColor = System.Drawing.Color.Black;
+            pctTitolo.Image = Properties.Resources.MagicSlot;
+
+            btnStart.ForeColor = System.Drawing.Color.FromArgb(178, 0, 77);
+            btnStop.ForeColor = System.Drawing.Color.FromArgb(178, 0, 77);
+            btnRisultati.ForeColor = System.Drawing.Color.FromArgb(178, 0, 77);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+
+            tempopartita.Start();         
+
+            bgwRullo1.RunWorkerAsync();
+            bgwRullo2.RunWorkerAsync();
+            bgwRullo3.RunWorkerAsync();          
+        }
+
+        private void bgwRullo1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            Thread.Sleep(500);
+            while (bgwRullo1.CancellationPending == false)
+            {
+                lock (rnd)
+                {
+                    NumRullo1 = rnd.Next(1, 5);
+                }
+
+                if (NumRullo1 == 1)
+                {
+                    pctRullo1.Image = Properties.Resources.Ciliegia;                    
+                }
+                else if (NumRullo1 == 2)
+                {
+                    pctRullo1.Image = Properties.Resources.Diamante;
+                }
+                else if (NumRullo1 == 3)
+                {
+                    pctRullo1.Image = Properties.Resources.Sette;
+                }
+                else if (NumRullo1 == 4)
+                {
+                    pctRullo1.Image = Properties.Resources.Uva;
+                }
+                Thread.Sleep(2000);
+                pctRullo1.Image = null;
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void bgwRullo2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(1000);
+            while(bgwRullo2.CancellationPending == false)
+            {
+                lock (rnd)
+                {
+                    NumRullo2 = rnd.Next(1, 5);
+                }
+
+                if (NumRullo2 == 1)
+                {
+                    pctRullo2.Image = Properties.Resources.Ciliegia;
+                }
+                else if (NumRullo2 == 2)
+                {
+                    pctRullo2.Image = Properties.Resources.Diamante;
+                }
+                else if (NumRullo2 == 3)
+                {
+                    pctRullo2.Image = Properties.Resources.Sette;
+                }
+                else if (NumRullo2 == 4)
+                {
+                    pctRullo2.Image = Properties.Resources.Uva;
+                }
+                Thread.Sleep(2000);
+                pctRullo2.Image = null;
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        private void bgwRullo3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(1500);
+            while (bgwRullo3.CancellationPending == false)
+            {
+                lock (rnd)
+                {
+                   NumRullo3 = rnd.Next(1, 5);
+                }
+
+                if (NumRullo3 == 1)
+                {
+                    pctRullo3.Image = Properties.Resources.Ciliegia;
+                }
+                else if (NumRullo3 == 2)
+                {
+                    pctRullo3.Image = Properties.Resources.Diamante;
+                }
+                else if (NumRullo3 == 3)
+                {
+                    pctRullo3.Image = Properties.Resources.Sette;
+                }
+                else if (NumRullo3 == 4)
+                {
+                    pctRullo3.Image = Properties.Resources.Uva;
+                }
+
+                //IF CHE VOLEVO USARE PER VERIFICARE IL JACKPOT:
+                //if ((pctRullo1.Image == pctRullo2.Image) && (pctRullo1.Image == pctRullo3.Image))
+                //{
+                //    pctTitolo.Image = null;
+                //    pctTitolo.Image = Properties.Resources.JACKPOT;
+                //}
+                if((NumRullo1 == NumRullo2)  && (NumRullo1 == NumRullo3))
+                {
+                    pctTitolo.Image = null;
+                    pctTitolo.Image = Properties.Resources.JACKPOT;
+                    
+                    numvittorie++;
+                }
+                Thread.Sleep(2000);
+                pctTitolo.Image = null;
+                pctTitolo.Image = Properties.Resources.MagicSlot;
+                pctRullo3.Image = null;
+                Thread.Sleep(1000);
+                numpartite++;
+                
+                
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {            
+            tempopartita.Stop();
+            bgwRullo1.CancelAsync();
+            bgwRullo2.CancelAsync();
+            bgwRullo3.CancelAsync();
+
+            //Faccio aspettare al bottone 3 secondi per far finire il lavoro ai bgw
+            //E per evitare errori
+            Thread.Sleep(3000);
+            btnStart.Enabled = true;
+        }
+        private void btnRisultati_Click(object sender, EventArgs e)
+        {
+            string messaggiomsgbox = "Numero Partite: " + numpartite + "\nPartite Vinte: " + numvittorie +"\nTempo di gioco:" + tempopartita.Elapsed.TotalSeconds + " Secondi";
+            string titolomsgbox = "Statistiche";
+
+            MessageBox.Show(messaggiomsgbox, titolomsgbox);
+        }
+    }
+}
